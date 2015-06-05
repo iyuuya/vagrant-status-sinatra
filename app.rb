dd2648a11f3eb47848b9950309436040019bf05a
iyuuya <vim.ito@1pac.jp>
2015-06-05 21:04:45 +0900

    table

--- app.rb
+++ app.rb
@@ -1,16 +1,49 @@
 require 'bundler'
 Bundler.require :default
 require 'sinatra/contrib/all'
+require 'sinatra/reloader'
+
+helpers do
+  def state_label(state)
+    label = case state
+            when /running/ then 'success'
+            when /poweroff/ then 'default'
+            else 'warning'
+            end
+    <<-TAG
+    <span class="label label-#{label}">#{state}</span>
+    TAG
+  end
+end
 
 get '/' do
-  @statuses = Vagrant.global_status
+  @machines = Vagrant::Machine.all
   erb :index
 end
 
-class Vagrant
-  class << self
-    def global_status
-      `vagrant global-status`
+module Vagrant
+  class Machine
+    ATTRIBUTES = %i(id name provider state directory)
+    attr_reader *ATTRIBUTES
+
+    def initialize(id: id, name: name, provider: provider, state: state, directory: directory)
+      @id = id
+      @name = name
+      @provider = provider
+      @state = state
+      @directory = directory
+    end
+
+    class << self
+      def all
+        result = []
+        `vagrant global-status`.each_line.to_a.map(&:chop).each_with_index do |line, i|
+          next if i < 2
+          break if line == " "
+          result << self.new(Hash[ATTRIBUTES.zip(line.split(/\s+/))])
+        end
+        result
+      end
     end
   end
 end
